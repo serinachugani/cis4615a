b44f141eeefd0429aa866bf31ff78cae1ae77e45
serinachugani <63365660+serinachugani@users.noreply.github.com>
2023-10-07 15:51:25 -0400

    Update R04_STR00_J.java
    change creation time of string until all the data is available

--- R04_STR00_J.java
+++ R04_STR00_J.java
@@ -5,14 +5,13 @@ public String readBytes(Socket socket) throws IOException {
   byte[] data = new byte[MAX_SIZE+1];
   int offset = 0;
   int bytesRead = 0;
-  String str = new String();
   while ((bytesRead = in.read(data, offset, data.length - offset)) != -1) {
-    str += new String(data, offset, bytesRead, "UTF-8");
     offset += bytesRead;
     if (offset >= data.length) {
       throw new IOException("Too much input");
     }
   }
+  String str = new String(data, 0, offset, "UTF-8");
   in.close();
   return str;
 }
