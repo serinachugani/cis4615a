0e7fd4a3f33cd0b15b660cee45eccf83715f999f
serinachugani <63365660+serinachugani@users.noreply.github.com>
2023-10-07 15:47:58 -0400

    Update R02_XP02_J.java
    Updated way to compare two arrays

--- R02_XP02_J.java
+++ R02_XP02_J.java
@@ -1,3 +1,3 @@
 int[] arr1 = new int[20]; // Initialized to 0
 int[] arr2 = new int[20]; // Initialized to 0
-System.out.println(arr1.equals(arr2)); // Prints false
+System.out.println(Arrays.equals(arr1, arr2)); // Prints true
